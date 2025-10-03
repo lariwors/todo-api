1220ba09c84da580faa051de4ea008f2c1c1faa3
lariwors <lariwors13@outlook.com>
2025-10-03 09:50:24 -0300

    feat: add expiration date

--- src/middlewares/tasksMiddleware.js
+++ src/middlewares/tasksMiddleware.js
@@ -1,11 +1,11 @@
 const validateBody = (request, response, next) => {
     const { body } = request
 
-function validateField(field, fieldName) {
-    if (!field || field === '') {
-        return response.status(400).json({ message: `The field "${fieldName}" is required and cannot be empty.` });
+    function validateField(field, fieldName) {
+        if (!field || field === '') {
+            return response.status(400).json({ message: `The field "${fieldName}" is required and cannot be empty.` });
+        }
     }
-}
 
 validateField(body.title, 'title');
 validateField(body.category, 'category');
--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -1,15 +1,17 @@
 const connection = require('./connection')
 
 const getAll = async () => {
-    const tasks =  await connection.execute("SELECT * FROM tasks")
-        return tasks
+    const [tasks] = await connection.execute("SELECT * FROM tasks")
+    return tasks
 }
 
 const addTask = async (task) => {
-    const { title, category, priority } = task
+    const { title, category, status, priority, expiration } = task
+    const now = new Date()
+    const createdAt = now.toLocaleString('sv-SE').replace(' ', ' ')
 
-    const query = 'INSERT INTO tasks (title, status, category, priority) VALUES (?, ?, ?, ?)'
-    const createdTask = await connection.execute(query, [title, 'Incomplete', category, priority])
+    const query = 'INSERT INTO tasks (title, status, category, priority, createdAt, expiration) VALUES (?, ?, ?, ?, ?, ?)'
+    const [createdTask] = await connection.execute(query, [title, status, category, priority, createdAt, expiration])
 
     return {insertId: createdTask.insertId}
 }
@@ -20,12 +22,12 @@ const deleteTask = async (id) => {
 }
 
 const updateTask = async (id, task) => {
-    const { title, status, category, priority } = task
+    const { status} = task
 
-    const query = 'UPDATE tasks SET title = ?, status = ?, category = ?, priority = ? WHERE id = ?'
+    const query = 'UPDATE tasks SET status = ? WHERE id = ?'
 
-    const updatedTask = await connection.execute(query, [title, status, category, priority, id])
-    return updatedTask    
+    const updatedTask = await connection.execute(query, [status, id])
+    return updatedTask
 }
 
 module.exports = {
